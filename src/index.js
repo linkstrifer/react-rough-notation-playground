c8fdd9093e9f61708c6bd163bb4763abd015a0ea
Link Strifer <linkstrifer@outlook.com>
2022-06-19 13:31:36 -0700

    update render to createroot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
+const rootElement = createRoot(document.getElementById("root"));
+rootElement.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
